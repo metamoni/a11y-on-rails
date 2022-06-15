30557924e4100ea31e38867a391b34e4df84a066
Monica Mateiu <monica.mateiu@gmail.com>
2022-06-15 19:26:07 +0200

    update validations and flash messages

--- spec/features/visit_validations_page_spec.rb
+++ spec/features/visit_validations_page_spec.rb
@@ -11,11 +11,10 @@ RSpec.describe 'Visit Validations page', type: :feature do
 
   it 'displays the correct headings', :aggregate_failures do
     expect(page.all('h2')[1]).to have_text 'The problem'
-    expect(page.all('h2')[2]).to have_text 'ARIA live regions'
-    expect(page.all('h2')[3]).to have_text 'Validation with HTML'
-    expect(page.all('h2')[4]).to have_text 'Try it yourself'
-    expect(page.all('h2')[5]).to have_text 'Custom form validation'
-    expect(page.all('h2')[6]).to have_text 'Resources'
+    expect(page.all('h2')[2]).to have_text 'Validation with HTML'
+    expect(page.all('h2')[3]).to have_text 'Try it yourself'
+    expect(page.all('h2')[4]).to have_text 'Custom form validation'
+    expect(page.all('h2')[5]).to have_text 'Resources'
   end
 
   it 'displays the "Back" link in the footer' do
