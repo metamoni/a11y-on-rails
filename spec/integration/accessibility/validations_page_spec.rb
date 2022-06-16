8fa749fcda9bca2f0e31e2c6c28f67e0df947c58
Monica Mateiu <monica.mateiu@gmail.com>
2022-06-16 09:50:05 +0200

    update validation accessibility specs

--- spec/integration/accessibility/validations_page_spec.rb
+++ spec/integration/accessibility/validations_page_spec.rb
@@ -1,6 +1,9 @@
 feature 'validations page', js: true do
   scenario 'is accessible' do
-    visit language_path
-    expect(page).to be_axe_clean
+    visit validations_path
+    expect(page).to be_axe_clean.excluding(
+      '#file-validation_with_aria_live-html-erb',
+      '#file-validation_with_role-html-erb'
+    )
   end
 end
